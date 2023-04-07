15be341af60389b67c7adb6a93c9f05034ef2f55
Dranikf <kobfedsur@gmail.com>
2023-04-07 12:31:39 +0300

    postgres_start готов!

--- docker/postgres_start/python_docker/script.py
+++ docker/postgres_start/python_docker/script.py
@@ -1,13 +1,23 @@
 import psycopg2
+import random
+import string
 
+# устанавливаю соединение с базой
 conn = psycopg2.connect(
-    port = ""
     dbname = "docker_app_db",
     user = "docker_app",
     password = "docker_app",
-    # host = "database"
+    host = "database"
 )
 
-conn.close()
+# вставляем в базу 20 случайных записей
+cur = conn.cursor()
+for i in range(20):
+    text = ''.join(random.choices(string.ascii_lowercase, k=20))
+    query = f"INSERT INTO main_table (id, text) VALUES ('{i}', '{text}');"
+    cur.execute(query)
+cur.close()
 
-print("hello world")
+# отсылаем зименения и закрываем соединение
+conn.commit()
+conn.close()
