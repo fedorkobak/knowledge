4356eef941108938ed91fe5d78e9cb3ab59af8b1
fedor kobak <kobfedsur@gmail.com>
2023-08-18 23:21:34 +0300

    python operators added

---
+++ postgres/load_tables/CSV/table_creation.sql
@@ -0,0 +1,13 @@
+CREATE TABLE main_table(
+    city VARCHAR(50),
+    city_ascii VARCHAR(50),
+    lat REAL,
+    lng REAL,
+    country VARCHAR(50),
+    iso2 VARCHAR(2),
+    iso3 VARCHAR(3),
+    admin_name VARCHAR(100),
+    capital VARCHAR(20),
+    population REAL,
+    id INT
+);
