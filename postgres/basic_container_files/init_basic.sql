63833842e53e152bba095047da581cb390b65404
Fedor Kobak <kobfedsur@gmail.com>
2023-09-02 23:54:18 +0300

    postgres NULL conditions

--- postgres/basic_container_files/init_basic.sql
+++ postgres/basic_container_files/init_basic.sql
@@ -18,3 +18,16 @@ INSERT INTO non_unique_values(col1, col2) VALUES
 ('B', 'X'),
 ('C', 'Y'),
 ('C', 'Y');
+
+
+CREATE TABLE table_with_nulls(
+    col1 TEXT,
+    col2 INT
+);
+INSERT INTO table_with_nulls(col1, col2) VALUES
+('A', NULL),
+('A', 1),
+('A', 2),
+(NULL, 1),
+('C', 3),
+(NULL, 4);
