be832964e46cdd0fb5770d8d3fe7d470623528e1
Fedor Kobak <kobfedsur@gmail.com>
2023-09-02 23:09:19 +0300

    SELECT DISTINCT added for postgres

--- postgres/basic_container_files/init_basic.sql
+++ postgres/basic_container_files/init_basic.sql
@@ -1,9 +1,20 @@
-CREATE TABLE main_table(
+CREATE TABLE simple_table(
     id TEXT NOT NULL,
     text TEXT NOT NULL
 );
-
-INSERT INTO main_table (id, text) VALUES
+INSERT INTO simple_table (id, text) VALUES
 (0, 'Text1'),
 (1, 'tExT2'),
 (3, 'TEXT3');
+
+CREATE TABLE non_unique_values(
+    col1 TEXT,
+    col2 TEXT
+);
+INSERT INTO non_unique_values(col1, col2) VALUES
+('A', 'X'),
+('A', 'Y'),
+('A', 'X'),
+('B', 'X'),
+('C', 'Y'),
+('C', 'Y');
