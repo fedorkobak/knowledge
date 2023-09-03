474a4417c896d41b3889c52b4dbfebd55d962ec9
Fedor Kobak <kobfedsur@gmail.com>
2023-09-03 11:46:01 +0300

    postgres section restrugtured

--- postgres/basic_container_files/init_basic.sql
+++ postgres/basic_container_files/init_basic.sql
@@ -6,28 +6,3 @@ INSERT INTO simple_table (id, text) VALUES
 (0, 'Text1'),
 (1, 'tExT2'),
 (3, 'TEXT3');
-
-CREATE TABLE non_unique_values(
-    col1 TEXT,
-    col2 TEXT
-);
-INSERT INTO non_unique_values(col1, col2) VALUES
-('A', 'X'),
-('A', 'Y'),
-('A', 'X'),
-('B', 'X'),
-('C', 'Y'),
-('C', 'Y');
-
-
-CREATE TABLE table_with_nulls(
-    col1 TEXT,
-    col2 INT
-);
-INSERT INTO table_with_nulls(col1, col2) VALUES
-('A', NULL),
-('A', 1),
-('A', 2),
-(NULL, 1),
-('C', 3),
-(NULL, 4);
