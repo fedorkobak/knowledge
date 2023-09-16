3009d15baac78700aeb454c25db15c8cecec8334
Fedor Kobak <kobfedsur@gmail.com>
2023-09-16 19:42:11 +0300

    python/fastapi   pass_arguments     get json as argument add

--- python/fastapi/pass_arguments_files/app.py
+++ python/fastapi/pass_arguments_files/app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    param1: int
+    param2: str
 
 app = FastAPI()
 
-@app.get("/divide")
-def divide(a : int, b : int) -> int:
-    return a/b
+@app.post("/")
+def read_json(item:Item):
+    return f"""I have got:
+    param1={item.param1};
+    param2={item.param2}."""
