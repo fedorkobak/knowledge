7dc6b43ee08a90e9ef18622c681c0f0eebd65d3b
Dranikf <kobfedsur@gmail.com>
2024-02-05 00:35:34 +0300

    python/advanced/unittest/moking   add section "Called with"

--- python/advanced/unittest/mocking_files/test_called_with.py
+++ python/advanced/unittest/mocking_files/test_called_with.py
@@ -3,12 +3,12 @@ from unittest.mock import patch
 from called_with import sum_wrapper
 
 class TestCalledWiht(unittest.TestCase):
-    def test_sum_wrapper1(self):
+    def test_ok(self):
         with patch("called_with.sum") as mocked_sum:
             sum_wrapper([1,2,3])
             mocked_sum.assert_called_with([1,2,3])
 
-    def test_sub_wrapper2(self):
+    def test_fail(self):
         with patch("called_with.sum") as mocked_sum:
             sum_wrapper([1,2,5])
             mocked_sum.assert_called_with([1,2,3])            
