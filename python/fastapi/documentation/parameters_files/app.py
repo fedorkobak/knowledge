31331abc60223d05e738c8dc3d91577cbdbe78f4
fedorkobak <kobfedsur@gmail.com>
2024-04-05 16:01:33 +0300

    python/fastapi/documentation/parameters   finish page and add it ot the toc

--- python/fastapi/documentation/parameters_files/app.py
+++ python/fastapi/documentation/parameters_files/app.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/{required}{no}")
-def say_hello(required : int, no=10):
+@app.get("/ex1/{par1}{par2=10}{par3}{par4=10}")
+def ex1(par1:int, par2:int, par3=10, par4=10):
+    return "hello"
+
+@app.get("/ex2")
+def ex2(par1:int, par2=10):
     return "hello"
