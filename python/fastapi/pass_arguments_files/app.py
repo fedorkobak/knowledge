75f183316337e678a0a0749c9ffcecbd091c61a1
fedorkobak <kobfedsur@gmail.com>
2024-04-05 16:21:06 +0300

    python/fastapi/pass_arguments   add path params section

--- python/fastapi/pass_arguments_files/app.py
+++ python/fastapi/pass_arguments_files/app.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    param1: int
-    param2: str
 
 app = FastAPI()
 
-@app.post("/")
-def read_json(item:Item):
-    return f"""I have got:
-    param1={item.param1};
-    param2={item.param2}."""
+@app.get("/divide/{a}/{b}")
+def divide(a:int, b:int) -> float:
+    return a/b
