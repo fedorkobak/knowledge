7dfd0384c07dce5e29fedcef3c79e0de49dbf06a
Dranikf <kobfedsur@gmail.com>
2024-04-18 21:10:01 +0300

    python/basics/logging   regroup basic logger vs cutiom logger   move exc_info to sparate subpage

---
+++ python/basics/logging_files/basic_logger.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
