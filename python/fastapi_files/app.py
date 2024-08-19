64f80792d8388e43a38d1e0d9fffda6fcc9f7dc1
Fedor Kobak <kobfedsur@gmail.com>
2024-08-19 23:49:18 +0300

    python/fastapi_files/app.py   finish "Cache" section

--- python/fastapi_files/app.py
+++ python/fastapi_files/app.py
@@ -1,20 +1,22 @@
+from random import random
+from collections.abc import AsyncIterator
+
 from fastapi import FastAPI
 
 from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
+from fastapi_cache.backends.redis import RedisBackend
 
-from redis import Redis
+from redis import asyncio as aioredis
 
-def lifespan(_: FastAPI) -> None:
-    redis = Redis.from_url("redis://localhost:6380")
+def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    redis = aioredis.from_url("redis://localhost:6380")
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
 
 app = FastAPI(lifespan=lifespan)
 
-from random import random
-
-@app.get("/")
-@cache(expire=60)
-def index():
+@app.get("/{id}")
+@cache(expire=600)
+def index(id: int):
     return random()
