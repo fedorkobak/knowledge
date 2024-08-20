a2f8f0db58312d150536caaa1357b2a89e887d49
fedorkobak <kobfedsur@gmail.com>
2024-08-20 14:40:29 +0300

    python/fastapi/cache.ipynb   get rid of extra

--- python/fastapi/cache_files/app.py
+++ python/fastapi/cache_files/app.py
@@ -1,5 +1,4 @@
 from random import random
-from collections.abc import AsyncIterator
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
@@ -10,12 +9,10 @@ from fastapi_cache.backends.redis import RedisBackend
 
 from redis import asyncio as aioredis
 
-def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url("redis://localhost:6380")
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-    yield
+redis = aioredis.from_url("redis://localhost:6380")
+FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 @app.get("/")
 @cache(expire=600)
