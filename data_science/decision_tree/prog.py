071ff72ae9d591d5b30e54252e6b514f10d7914b
fedorkobak <kobfedsur@gmail.com>
2024-11-26 16:48:44 +0300

    data_science/decision_tree.ipynb add compution of GINI for subsets

--- data_science/decision_tree/prog.py
+++ data_science/decision_tree/prog.py
@@ -24,10 +24,10 @@ def plot_Q_scatter(
         if sep_orient == 'h':
             plt.axhline(y=sep_val, color="gray", linestyle="--")
             x_text_left, y_text_left, x_text_right, y_text_right = (
-                (arrange[1] + arrange[0]) / 2,
-                (arrange[3] + sep_val) / 2,
                 (arrange[1] + arrange[0]) / 2, 
                 (arrange[2] + sep_val) / 2,
+                (arrange[1] + arrange[0]) / 2,
+                (arrange[3] + sep_val) / 2,
             )
         else:
             plt.axvline(x=sep_val, color="gray", linestyle="--")
