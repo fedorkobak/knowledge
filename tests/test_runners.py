cd716faaecc161b67e705a7d66ef40dc2fa69f17
fedor <kobfedsur@gmail.com>
2025-05-01 22:59:56 +0300

    set global runners for test execute output

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -11,20 +11,26 @@ class TestExecuteOutput(TestCase):
     """
     Test the execute output of the database runners.
     """
-    def test_simple_query(self):
-        runners = [
-            PostgresRunner,
-            ClickHouseRunner,
-            SQLiteRunner
+    @classmethod
+    def setUpClass(cls):
+        cls.runners = [
+            PostgresRunner(),
+            ClickHouseRunner(),
+            SQLiteRunner()
         ]
+
+    @classmethod
+    def tearDownClass(cls):
+        for runner in cls.runners:
+            runner.stop()
+
+    def test_simple_query(self):
         query = "SELECT 1 AS value, 2 AS value2;"
         exp_cols = ("value", "value2")
         exp_data = ((1, 2),)
 
-        for runner_class in runners:
-            r = runner_class()
+        for r in self.runners:
             ans_cols, ans_data = r.execute(query)
             fail_msg = f"Failed for {r.__class__.__name__}"
             self.assertEqual(ans_cols, exp_cols, msg=fail_msg)
             self.assertEqual(ans_data, exp_data, msg=fail_msg)
-            r.stop()
