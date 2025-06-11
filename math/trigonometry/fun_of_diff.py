7d9d45d2da8f1b914fe188b1cdd77057abc4eee3
fedor <kobfedsur@gmail.com>
2025-06-12 00:21:08 +0300

    make points size smaller

--- math/trigonometry/fun_of_diff.py
+++ math/trigonometry/fun_of_diff.py
@@ -11,12 +11,12 @@ viewbox = f"-0.1 -0.1 {w + mul * w} {h + mul * h}"
 template = f"""
 <svg width="{w * 600}" height="{h * 600}" viewbox="{viewbox}">
     <g>
-        <circle cx="0" cy="0" r="0.015" />
-        <circle cx="0" cy="{h}" r="0.015" />
-        <circle cx="{w}" cy="0" r="0.015" />
-        <circle cx="{w}" cy="{h}" r="0.015" />
-        <circle cx="{m.cos(a)*m.cos(b)}" cy="0" r="0.015" />
-        <circle cx="{w}" cy="{m.cos(a)*m.sin(b)}" r="0.015" />
+        <circle cx="0" cy="0" r="0.012" />
+        <circle cx="0" cy="{h}" r="0.012" />
+        <circle cx="{w}" cy="0" r="0.012" />
+        <circle cx="{w}" cy="{h}" r="0.012" />
+        <circle cx="{m.cos(a)*m.cos(b)}" cy="0" r="0.012" />
+        <circle cx="{w}" cy="{m.cos(a)*m.sin(b)}" r="0.012" />
     </g>
     <path
         d="M 0 0 L 0 0 0 {h} {w} {h} {w} 0 Z"
