8ad368c8b827dfa28e870993ca60899739246b99
fedorkobak <kobfedsur@gmail.com>
2025-11-29 22:59:55 +0300

    make gitkernel inherit from tempfile

--- src/git_kernel/git_kernel.py
+++ src/git_kernel/git_kernel.py
@@ -1,32 +1,20 @@
-from pathlib import Path
-from command_kernel import CommandKernel, command
-from jinja2 import Environment, FileSystemLoader
+from command_kernel import command
+from tempdir_kernel import TempDirKernel
 
 
-class GitKernel(CommandKernel):
+class GitKernel(TempDirKernel):
     '''
     The kernel that hides the process of creating a git repo.
     To start working with a new git repo,
     simply start the cell with the "magic" `%init`.
     '''
 
-    repo_path = "/tmp/git_temp"
-
-    @command('%init')
-    def _init_new_repo(self, code: str = '') -> str:
-        command = self._render_script()
-        self.bashwrapper.run_command(command=command)
+    @command('#init')
+    def _init_dir(self, code: str = '') -> str:
+        super()._init_dir()
+        self.bashwrapper.run_command("git init")
         return code
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._init_new_repo('')
-
-    def _render_script(self) -> str:
-        '''
-        Builds the script that creates a new git repo.
-        '''
-        path = Path(__file__)
-        env = Environment(loader=FileSystemLoader(path.parent))
-        template = env.get_template("create_repo.sh.j2")
-        return template.render(repo_path=self.repo_path)
+        self._init_dir('')
