94660b692bd588e593189e503ea0869a1ac84a18
fedorkobak <kobfedsur@gmail.com>
2026-02-20 12:39:11 +0100

    generalize the get_admin_token approach

--- infrastructure/keycloak_files/utils.sh
+++ infrastructure/keycloak_files/utils.sh
@@ -1,9 +1,8 @@
 get_admin_token() {
-    access_token=$(curl -X POST -s \
+    echo $(curl -X POST -s \
         -d "grant_type=password"\
         -d "client_id=admin-cli" \
         -d "username=admin" \
         -d "password=admin" \
-        http://localhost:8080/realms/master/protocol/openid-connect/token | jq -r .access_token)
-    echo $access_token
+        http://localhost:8080/realms/master/protocol/openid-connect/token)
 }
