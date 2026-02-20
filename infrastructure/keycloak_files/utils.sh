1fc48a975252fa3f2ab90aa4d83e87fa8f603b13
fedorkobak <kobfedsur@gmail.com>
2026-02-20 14:04:59 +0100

    add short cut for user data

--- infrastructure/keycloak_files/utils.sh
+++ infrastructure/keycloak_files/utils.sh
@@ -6,3 +6,15 @@ get_admin_token() {
         -d "password=admin" \
         http://localhost:8080/realms/master/protocol/openid-connect/token)
 }
+
+get_admin_user() {
+    # Get the information of admin user
+    # you can optionally pass the access token
+    # otherwise the new session will be created
+    local access_token=${1:-$(get_admin_token | jq -r .access_token)}
+    echo $(
+        curl -X GET -s \
+            -H "authorization: Bearer $access_token" \
+            http://localhost:8080/admin/realms/master/users?username=admin | jq .[0]
+    )
+}
